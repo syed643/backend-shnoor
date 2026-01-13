dfbc3787fbfcbcda898df87c034d5f8e53d5d731
syed643 <syed@shnoor.com>
2026-01-13 14:44:35 +0530

    c

--- db/postgres.js
+++ db/postgres.js
@@ -2,20 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-(async () => {
-  try {
-    await pool.query("SELECT 1");
-    console.log("✅ PostgreSQL connected");
-  } catch (err) {
-    console.error("❌ PostgreSQL connection failed", err);
-  }
-})();
 
 export default pool;
