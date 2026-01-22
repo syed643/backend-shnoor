80d49b5b8ba315a4d38acf7896056c17eeabaac4
syed643 <syed@shnoor.com>
2026-01-22 13:18:01 +0530

    c

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -239,7 +239,7 @@ export const getCourseById = async (req, res) => {
         c.courses_id,
         c.title,
         c.description,
-        c.level,
+        c.difficulty,
         c.thumbnail_url,
         c.created_at,
         u.full_name AS instructor_name
