acc897cd533880baf4e555da822a04885247bcdc
syed643 <syed@shnoor.com>
2026-01-25 13:34:28 +0530

    c

--- config/mailer.js
+++ config/mailer.js
@@ -1,13 +1,16 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false, // true only for 465
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // MUST be false
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    rejectUnauthorized: false, // IMPORTANT on Render
+  },
+  connectionTimeout: 10000,
 });
-
 export default transporter;
