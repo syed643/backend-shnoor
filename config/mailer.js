5f25731ef462a271289579ac9a1622d34c9c57e4
syed643 <syed@shnoor.com>
2026-01-30 19:40:12 +0530

    c

--- config/mailer.js
+++ config/mailer.js
@@ -8,6 +8,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  family: 4,
   tls: {
     rejectUnauthorized: false, // IMPORTANT on Render
   },
