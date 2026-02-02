238aaf718fcd401d284e121c46f197091813c04b
syed643 <syed@shnoor.com>
2026-02-02 18:45:08 +0530

    C

--- config/mailer.js
+++ config/mailer.js
@@ -3,7 +3,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendMail = async ({ to, subject, html }) => {
   try {
     const response = await resend.emails.send({
-      from: "no-reply@lms.shnoor.com", 
+      from: "LMS-SHNOOR@lms.shnoor.com", 
       to,
       subject,
       html,
